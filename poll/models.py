db8adc016e94bdeee57652a26feecae9c5dc8323
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-08-06 15:54:09 +0330

    coding user model and migrate

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import UserManager
+
 
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(max_length=11, unique=True)
+    first_name = models.CharField(max_length=250, blank=True, null=True)
+    last_name = models.CharField(max_length=250, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
