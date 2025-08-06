bbbae955659d24ed892050785f5778a75c0a743d
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-08-06 17:11:51 +0330

    coding simple viewset for poll

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
+
+
+class PollViewSet(ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    permission_classes = [IsAuthenticated]
