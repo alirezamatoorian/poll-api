d3dba39161d58ee1beeeb1590e3d719ad2befef2
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-08-06 15:15:01 +0330

    chane database to postresql

--- poll_api/settings.py
+++ poll_api/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'poll_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pollapp',
+        'USER': 'pollapp_admin',
+        'PASSWORD': 'shahrivar1380',
+        'PORT': '5432'
     }
 }
 
