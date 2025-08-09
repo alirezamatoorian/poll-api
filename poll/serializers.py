84a44d0e10c90001062e27e20f23b24bb9cd6244
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-08-09 15:54:16 +0330

    coding more validation for poll serilaizer

--- poll/serializers.py
+++ poll/serializers.py
@@ -1,21 +1,38 @@
 from rest_framework import serializers
 from .models import *
+from django.utils import timezone
 
 
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
-        fields = '__all__'
+        fields = ['text', 'poll']
+        read_only_fields = ['poll']
 
 
 class PollSerializer(serializers.ModelSerializer):
     choices = ChoiceSerializer(many=True)
 
     class Meta:
-        mode = Poll
+        model = Poll
         fields = '__all__'
 
     def validate(self, attrs):
-        if attrs['start_date'] >= attrs['end_date']:
+        start_date = attrs['start_date']
+        end_date = attrs['end_date']
+        if start_date >= end_date:
             raise serializers.ValidationError("تاریخ شروع باید قبل از تاریخ پایان باشد")
+        if end_date <= start_date:
+            raise serializers.ValidationError('تاریخ پایان باید بعد از تاریخ شروع باشد')
+        if start_date <= timezone.now():
+            raise serializers.ValidationError("تاریخ شروع باید بعد از زمان فعلی باشد")
         return attrs
+
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices')
+        poll = Poll.objects.create(**validated_data)
+
+        for choice_data in choices_data:
+            Choice.objects.create(poll=poll, **choice_data)
+
+        return poll
